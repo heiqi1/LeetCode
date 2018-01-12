dd3a8f5fb48757b2e14983e5c024a6ae58df706e
ken <389079588@qq.com>
2018-01-12 18:59:16 +0800

    Create 217_ContainsDuplicate.java

---
+++ 201-250/217_ContainsDuplicate.java
@@ -0,0 +1,15 @@
+```
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        if (nums==null||nums.length==0)
+            return false;
+        for (int i=0;i<nums.length;i++){
+            if (!set.add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+}
+
+```
