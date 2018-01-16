842848828edd474522f57e889e203f5fab06dfcf
ken <389079588@qq.com>
2018-01-16 11:26:28 +0800

    Update Solution.java

--- 201-250/222. Count Complete Tree Nodes/Solution.java
+++ 201-250/222. Count Complete Tree Nodes/Solution.java
@@ -3,6 +3,7 @@ public class Solution {
         return countNodes(root,-1,-1);
     }
     public int countNodes(TreeNode root,int left,int right){
+         // 如果没有上轮计算好的左子树深度，计算其深度
         if(left==-1){
             TreeNode cur=root;
             left=0;
@@ -11,6 +12,7 @@ public class Solution {
                 cur=cur.left;
             }
         }
+        // 如果没有上轮计算好的右子树深度，计算其深度
         if(right==-1){
             TreeNode cur=root;
             right=0;
@@ -19,7 +21,9 @@ public class Solution {
                 cur=cur.right;
             }
         }
+        // 如果两个深度一样，返回2^h-1
         if(left==right) return (1<<left)-1;
+         // 否则返回左子树右子树节点和加1
         return 1+countNodes(root.left,left-1,-1)+countNodes(root.right,-1,right-1);
     }
 }
