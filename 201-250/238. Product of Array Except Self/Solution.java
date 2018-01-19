e3e434d32571645ac90881e2c37309b9622631d5
ken <389079588@qq.com>
2018-01-19 16:39:27 +0800

    Create Solution.java

---
+++ 201-250/238. Product of Array Except Self/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0) return nums;
+        int len=nums.length;
+        int[] result=new int[len];
+        int left=1;
+        for(int i=0;i<len;i++){
+            result[i]=left;
+            left*=nums[i];
+        }
+        int right=1;
+        for(int i=len-1;i>=0;i--){
+            result[i]*=right;
+            right*=nums[i];
+        }
+        return result;
+    }
+}
