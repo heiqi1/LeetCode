268acf36f3e9f2b14fb2878d332dcc99b40cfc05
ken <389079588@qq.com>
2018-01-19 16:22:54 +0800

    Create Solution 2.java

---
+++ 151-200/169. Majority Element/Solution 2.java
@@ -0,0 +1,21 @@
+```
+public class Solution2 {
+    public int majorityElement(int[] nums) {
+        int elem=0;
+        int count=0;
+        for (int i=0;i<nums.length;i++){
+            if (count==0){
+                elem=nums[i];
+                count=1;
+            }else {
+                if (elem==nums[i])
+                    count++;
+                else 
+                    count--;
+            }
+        }
+        return elem;
+    }
+}
+
+```
