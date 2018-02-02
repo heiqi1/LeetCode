11e9422599eddda425ea4851254ef83f76b0195f
ken <389079588@qq.com>
2018-02-02 11:01:52 +0800

    Create Solution.java

---
+++ 1-50/26. Remove Duplicates from Sorted Array/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeDuplicates(int[] nums){
+        if (nums.length==0)
+            return 0;
+        int length=1;
+        for (int i=1;i<nums.length;i++){
+            if (nums[i]!=nums[i-1])
+                nums[length++]=nums[i];
+        }
+        return length;
+    }
+}
