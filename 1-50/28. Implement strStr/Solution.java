ca6ddd81194bddf950c20ad2dc3805d58bae6a30
ken <389079588@qq.com>
2018-02-03 13:50:11 +0800

    Create Solution.java

---
+++ 1-50/28. Implement strStr/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack==null)return -1;
+        if(needle==null)return -1;
+        if(haystack.length()<needle.length())return -1;
+        int len=haystack.length()-needle.length();
+        for(int i=0;i<=len;i++){
+            int j;
+            for(j=0;j<needle.length();j++){
+                if(haystack.charAt(i+j)!=needle.charAt(j))
+                    break;
+            }
+            if(j==needle.length())return i;//找到匹配
+        }
+        return -1;
+    }
+}
